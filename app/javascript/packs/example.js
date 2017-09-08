e8eaf0d71589ae47ff6d977c7ade6af8c69e2285
Greg Nagy <gregnagy10@gmail.com>
2017-09-08 16:50:19 -0500

    Remove react-rails, put in javascript_pack_tag in layout

---
+++ app/javascript/packs/example.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Example extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>Made it!</p>
+      </div>
+    )
+  }
+
+}
+
+export default Example
