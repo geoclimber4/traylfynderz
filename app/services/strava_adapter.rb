d9a49d2ef86b76b15e83cb0e37e505579925ba1b
George <georgedzuricsko1@gmail.com>
2017-09-08 19:58:03 -0500

    refactored parsing method out of controller and in to strava

--- app/services/strava_adapter.rb
+++ app/services/strava_adapter.rb
@@ -7,10 +7,30 @@ class StravaAdapter
     @client = Strava::Api::V3::Client.new(:access_token => ENV["ACCESS_TOKEN"])
   end
 
-  # https://www.strava.com/oauth/authorize?
-  #   client_id=9
-  #   &response_type=code
-  #   &redirect_uri=http://testapp.com/token_exchange
+  # activity_type:	string optional
+  # ‘running’ or ‘riding’, default is riding
+  # bounds must be ‘sw.lat,sw.lng,ne.lat,ne.lng’
+  def find_routes(args = {})
+    segment_data = self.client.segment_explorer( bounds: [41.699412,-87.920408,41.725388,-87.885764], activity_type: 'running')
+    segments = []
+    segment_data["segments"].each do |seg|
+      segments << Segment.create(
+        strava_id: seg["id"],
+        name: seg["name"],
+        distance: seg["distance"],
+        average_grade: seg["avg_grade"],
+        start_lat: seg["start_latlng"][0],
+        start_long: seg["start_latlng"][1],
+        end_lat: seg["end_latlng"][0],
+        end_long: seg["end_latlng"][1],
+        avg_grade: seg["avg_grade"],
+        climb_category_desc: seg["climb_category_desc"],
+        climb_category: seg["climb_category"]
+        )
+    end
+    return segments
+  end
+
 end
 
   # @client.retrieve_segment_streams(:segment_id)
