c2827a54553e026a20fe59a1448aa4236bc34e76
Greg Nagy <gregnagy10@gmail.com>
2017-09-09 10:42:02 -0500

    Delete extra segment controller

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -2,27 +2,17 @@
 
 class SegmentsController < ApplicationController
 
-  # def index
-  #   @segments = Segment.all
-  #   respond_to do |f|
-  #     f.json { render :json => @segments }
-  #   end
-  # end
-
-  def index
-<<<<<<< HEAD
+def index
     segments = Segment.all
     respond_to do |format|
       format.json { render :json => segments }
     end
-  end
-=======
->>>>>>> master
+end
 
     @adapter = StravaAdapter.new
     p "right route"
 
-    @segments = @adapter.find_routes  
+    @segments = @adapter.find_routes
     p @segments
     render json: @segments
   end
