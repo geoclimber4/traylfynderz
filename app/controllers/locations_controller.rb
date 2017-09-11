14d41982755af9145f86ad97547a336d79392821
Greg Nagy <gregnagy10@gmail.com>
2017-09-10 20:51:18 -0500

    return geojson with modified locations controller. kept for code history purposes. Do not mergegit add .git add .!

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,22 @@
+require 'overpass_api_ruby'
 
 class LocationsController < ApplicationController
 
   def new
-    @location =
+    @location = Location.create({address: "141 Willows Edge Ct. Willow Springs, IL 60480"})
+    options={:bbox => {:s => -34.705448, :n => -34.526562,
+                   :w => -58.531471, :e => -58.335159},
+             :timeout => 900,
+             :element_limit => 1073741824}
+
+    overpass = OverpassAPI::XML.new(options)
+
+    query = "<union><query type='relation'><has-kv k='route' v='subway'/></query>" <<
+            "</union><union><item/><recurse type='down'/></union>"
+
+    response = overpass.query(query)
+
+    render json: response
   end
 
   def show
