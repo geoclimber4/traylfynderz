b21d6b7245211df47148be0d552b1c50466a6971
Greg Nagy <gregnagy10@gmail.com>
2017-09-12 14:59:54 -0500

    More location model tests

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 describe Location do
-  let(:location) { Location.new({"Utica, IL"})}
+  let(:location) { Location.create!(address: "Utica, IL")}
 
-  describe "segments" do
-    it "has a collection of segments" do
-      expect(location.segments).to be_an_instance_of(Array)
+  describe "address" do
+    it "has an address in string form" do
+      p location
+      location.segments.each do |segment|
+        p segment.first.name
+        p segment.first.distance
+      end
+      expect(location.address).to be_an_instance_of(String)
     end
   end
 
+  describe "coordinates" do
+    it "has a latitude" do
+      expect(location.latitude).to be_an_instance_of(Float)
+    end
 
+    it "has a longitude" do
+      expect(location.longitude).to be_an_instance_of(Float)
+    end
+  end
 
 end
