517388263af5017b2bd5fefbf2bbe7368304d29b
George Dzuricsko <email": null>
2017-09-11 20:25:48 -0500

    current save point with heavy modification to locations_controller for testing only. do not merge with good copy

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,19 +4,19 @@ class LocationsController < ApplicationController
 
   def new
     @location = Location.create({address: "141 Willows Edge Ct. Willow Springs, IL 60480"})
-    options={:bbox => {:s => -34.705448, :n => -34.526562,
-                   :w => -58.531471, :e => -58.335159},
+    options={:bbox => {:s => 41.75, :n => 41.8, :w => -87.9, :e => -87.8},
              :timeout => 900,
              :element_limit => 1073741824}
 
     overpass = OverpassAPI::XML.new(options)
 
-    query = "<union><query type='relation'><has-kv k='route' v='subway'/></query>" <<
+    query = "<union><query type='way'><has-kv k='highway' v='footway'/></query>" <<
             "</union><union><item/><recurse type='down'/></union>"
 
     response = overpass.query(query)
+    p response[:elements]
 
-    render json: response
+    render json: response[:elements]
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909012902) do
+ActiveRecord::Schema.define(version: 20170911164436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20170909012902) do
     t.decimal "end_lat"
     t.decimal "end_long"
     t.decimal "avg_grade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trails", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
