1cb06149d1354284da407d0173f86d4af335e285
Greg Nagy <gregnagy10@gmail.com>
2017-09-12 11:49:11 -0500

    Start Location model tests failing

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 describe Location do
-  let(:location)
+  let(:location) { Location.new({"Utica, IL"})}
 
-
+  describe "segments" do
+    it "has a collection of segments" do
+      expect(location.segments).to be_an_instance_of(Array)
+    end
+  end
 
 
 
