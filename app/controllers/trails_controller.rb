5c708b27291db800aca47cc9832da67a59779788
George <georgedzuricsko1@gmail.com>
2017-09-12 14:16:24 -0500

    update controller to query new location

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -3,8 +3,17 @@ class TrailsController < ApplicationController
   def new
     p "trails new"
     @adapter = OverpassAdapter.new
-
-    render json: @adapter.find_trails
+    @location = Location.create(address: "Moab, UT")
+    p @location
+    geojson = @adapter.run_query(
+          swlat: (@location.latitude - 0.1),
+          swlng: (@location.longitude - 0.1 ),
+          nelat: (@location.latitude + 0.1),
+          nelng: (@location.longitude + 0.1))
+    p geojson
+    @trails = @adapter.find_trails(geojson)
+    p @trails
+    render json: @trails.to_json
   end
 
 end
--- app/services/overpass_adapter.rb
+++ app/services/overpass_adapter.rb
@@ -3,18 +3,36 @@ require 'overpass_api_ruby'
 class OverpassAdapter
 
 # rewrite this to take arguments, possibly refactor to trail model
-  def find_trails
-    json = self.run_query
-    return json
+  def find_trails(geojson)
+    ways = geojson[:elements].select {|hashie_guy|  hashie_guy[:type] == "way"  }
+    trails = ways.map do |item|
+      p item
+      Trail.create(
+        name: item[:tags][:name],
+        feature_id: item[:id],
+        bicycle: item[:tags][:bicycle],
+        foot: item[:tags][:foot],
+        owner: item[:tags][:owner],
+        surface: item[:tags][:surface],
+        geo: item[:nodes]
+        )
+    end
+
+    return trails
   end
 
 
 
-  def run_query
-    options={:bbox => {:s => 41.69592025780443,
-                       :n => 41.705119764133286,
-                       :w => -87.89371490478516,
-                       :e => -87.88287878036499},
+  def run_query(args )
+      south = args[:swlat] || 41.6959
+      north = args[:nelat] || 41.70511
+      east = args[:swlong] || -87.8828
+      west = args[:nelong] || -87.8937
+
+    options={:bbox => {:s => south,
+                       :n =>  north,
+                       :w => west,
+                       :e => east },
              :timeout => 900,
              :element_limit => 1073741824}
 
