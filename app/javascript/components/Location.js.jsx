f5e2b180b3adefee4719028ea2b4a7b756ba4d4c
Greg Nagy <gregnagy10@gmail.com>
2017-09-13 17:05:15 -0500

    Pull old commented out code in Location.jsx

--- app/javascript/components/Location.js.jsx
+++ app/javascript/components/Location.js.jsx
@@ -6,19 +6,6 @@ export default class Location extends React.Component {
     this.state = {location: [] }
     console.log(this.state)
   }
-  // Should return xhr for
-  // componentDidMount() {
-  //   console.log('Component verily did mount')
-  //   $.getJSON('location.json', (response) => { this.setState({
-  //     location: response })
-  //     console.log("put json here")
-  //   }).done(function(response){
-  //     console.log(response);
-  //   }).fail(function(response){
-  //     console.log("no funsies")
-  //     console.log(response)
-  //   })
-  // }
 
   render() {
     if (this.props.location.length > 0) {
@@ -42,21 +29,6 @@ export default class Location extends React.Component {
       </div>
       )
   }
-    // var segments= this.state.segments.map((segment) => {
-    //   console.log(segment)
-    //   return (
-    //     <div key={segment.id}>
-    //       <h5>-{segment.name}-</h5>
-    //       <p>{segment.distance} longboys</p>
-    //     </div>
-    //     // React.createElement('h1', null, '${segment.name} - {segment.distance}')
-    //   )
-    // })
 
-    // return (
-    //   <div>
-    //     {segments}
-    //   </div>
-    // )
-  }
-// }
+}
+
